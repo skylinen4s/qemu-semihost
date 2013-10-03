b7853fc47204108c2e113847efbc26a13724271a
skylinen4s <skyline_n4s@hotmail.com>
2013-10-04 00:15:29 +0800

    Do multiplication and print the result to the file.

--- ftest.c
+++ ftest.c
@@ -4,6 +4,7 @@ int main ()
 {
 	FILE *pFile;
 	int buf_leng = 100;
+	int num ,result;
 	char buf[buf_leng];
 
 	pFile = fopen ("host.txt","r+");
@@ -15,12 +16,28 @@ int main ()
   		printf("%s\n",buf);
   		printf("---------------------Over--------------------\n");
   	}
-  	if (pFile == NULL)
+	if (pFile == NULL)
 	{
   		pFile = fopen ("host.txt","w");
   		printf("No file. Create a new file\n");
 	}
-    	fclose (pFile);
+  		printf("this program will multiple\n");
+  		printf("please enter a new number\n");
+		scanf("%d", &num);
 
+		int count = 0;
+		while(count < num)
+		{
+			result = num * (++count);
+			printf("%d * %d = %d\n", num, count, result);
+			fprintf(pFile, "%d * %d = ", num, count);
+			fprintf(pFile,"%d\n", result);
+		}
+    	fclose (pFile);
+	
+
+	//pFile = fopen ("host.txt","a");
+	//fprintf(pFile,"%d\n",result);
+	//fclose(pFile);
 	return 0;
 }
